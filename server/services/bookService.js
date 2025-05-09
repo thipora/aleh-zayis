303da6fb01a939d05e217175d8936434d725c622
TzipiPasternak <tz0556776105@gmail.com>
2025-05-09 14:12:47 +0300

    get books from db (not from CLICKUP)

--- server/services/bookService.js
+++ server/services/bookService.js
@@ -1,6 +1,8 @@
 import { ClickUpService } from './clickup/clickUpService.js';
 import { EmployeeService } from './employeesService.js';
 import { SimpleCache } from '../until/simpleCache.js';
+import { executeQuery } from "../config/db.js";
+
 
 const booksCache = new SimpleCache(1000 * 60 * 25);
 
@@ -14,25 +16,45 @@ export class BooksService {
         this.editorFieldName = 'Editor Information';
     }
 
-    async getBooksForWorker(workerId, { start = 0, range = 10, sort = "name ASC" } = {}) {
-        const employeeService = new EmployeeService();
-        const employeeClickUpId = await employeeService.getClickUpIdByWorkerId(workerId);
+    // async getBooksForWorker(workerId, { start = 0, range = 10, sort = "name ASC" } = {}) {
+    //     const employeeService = new EmployeeService();
+    //     const employeeClickUpId = await employeeService.getClickUpIdByWorkerId(workerId);
     
+    //     const [sortField, sortOrder] = sort.split(' ');
+    //     const validSortField = ['name', 'id_book'].includes(sortField) ? sortField : 'name';
+    //     const validSortOrder = sortOrder?.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
+    
+    //     const query = `
+    //         SELECT id_book, name AS title
+    //         FROM books
+    //         WHERE project_manager_clickup_id = ?
+    //         ORDER BY ${validSortField} ${validSortOrder}
+    //         LIMIT ?, ?
+    //     `;
+    
+    //     const books = await executeQuery(query, [employeeClickUpId, start, range]);
+    //     return books;
+    // }
+
+    async getBooksForWorker(workerId, { sort = "name ASC" } = {}) {
         const [sortField, sortOrder] = sort.split(' ');
         const validSortField = ['name', 'id_book'].includes(sortField) ? sortField : 'name';
         const validSortOrder = sortOrder?.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
-    
+      
         const query = `
-            SELECT id_book, name AS title
-            FROM books
-            WHERE project_manager_clickup_id = ?
-            ORDER BY ${validSortField} ${validSortOrder}
-            LIMIT ?, ?
+          SELECT DISTINCT books.id_book, books.name AS title
+          FROM books
+          JOIN book_assignments ON books.id_book = book_assignments.book_id
+          JOIN employee_roles ON book_assignments.employee_role_id = employee_roles.id_employee_role
+          WHERE employee_roles.employee_id = ?
+        ORDER BY ${validSortField} ${validSortOrder}
         `;
-    
-        const books = await executeQuery(query, [employeeClickUpId, start, range]);
+
+      
+        const books = await executeQuery(query, [workerId]);
         return books;
-    }
+      }
+      
     
 
     async getAllBooks() {
