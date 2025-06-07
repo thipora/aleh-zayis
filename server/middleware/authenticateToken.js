a047df55fccd85024c972c3790d5afe831919f2a
TzipiPasternak <tz0556776105@gmail.com>
2025-06-07 22:52:31 +0300

    change verify Token that chek not whith cookies

--- server/middleware/authenticateToken.js
+++ server/middleware/authenticateToken.js
@@ -3,14 +3,16 @@ import jwt from 'jsonwebtoken';
 const secret = process.env.ACCESS_TOKEN_SECRET;
 
 export const verifyToken = (req, res, next) => {
-    const token = req?.query?.token || req?.cookies["x-access-token"];
+    const authHeader = req.headers['authorization'];
+    const tokenFromHeader = authHeader && authHeader.split(' ')[1];
+    const token = tokenFromHeader || req?.query?.token || req?.cookies["x-access-token"];
     console.log(token)
     if (!token)
         return res.sendStatus(403).send("not access Token");
 
     try {
         const verified = jwt.verify(token, secret);
-        req.body.userId = verified.id;
+        req.body.userId = verified.idUser || verified.id;
         return next();
     } catch (error) {
         return res.status(401).send("Invalid Token");
