393cb647b16d3b4b1f2be4033322b879efbf0e0d
TzipiPasternak <tz0556776105@gmail.com>
2025-06-09 12:31:23 +0300

    change limit rate to 10

--- server/middleware/rateLimit.js
+++ server/middleware/rateLimit.js
@@ -2,6 +2,6 @@ import rateLimit from 'express-rate-limit';
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 5,
+  max: 10,
   message: "Too many attempts, please try again later."
 });
